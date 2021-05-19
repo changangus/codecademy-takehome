7432565ac7ab497297cd222b4e39e89353a9246b
changangus <63685366+changangus@users.noreply.github.com>
2021-05-19 10:29:52 -0400

    fixed persisting wrong answer styles bug

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -16,19 +16,11 @@ const Question: React.FC<QuestionProps> = ({ question: { text, correctAnswer, in
   const isSubmitted = useSelector<RootStore, questionState["isSubmitted"]>(state => state.question.isSubmitted);
   let [answers, setAnswers] = useState<string[]>([]);
   const dispatch = useDispatch();
-  const isCorrect = submittedAnswer === correctAnswer;
 
   useEffect(() => {
     const shuffledAnswers = shuffleAnswers([correctAnswer, ...incorrectAnswers]);
     setAnswers(shuffledAnswers)
   }, [correctAnswer, incorrectAnswers]);
-
-  const clickedButton = document.getElementById(submittedAnswer);
-  if (clickedButton && !isCorrect && isSubmitted) {
-    clickedButton.style.border = '2px solid red';
-    clickedButton.style.background = 'pink';
-    clickedButton.style.textDecoration = 'line-through';
-  }
 
   return (
     <div>
@@ -36,7 +28,7 @@ const Question: React.FC<QuestionProps> = ({ question: { text, correctAnswer, in
       <form className="flex flex-col">
         {answers.map(answer => (
           <input
-            className={`text-xl my-4 p-2 rounded-xl bg-gray-100 border-solid border-gray-500 border-2 transition-all cursor-pointer ${isSubmitted && answer === correctAnswer ? 'border-solid border-2 border-green-400 bg-green-200' : ''}`}
+            className={`text-xl my-4 p-2 rounded-xl bg-gray-100 border-solid border-gray-500 border-2 transition-all cursor-pointer ${isSubmitted && answer === correctAnswer ? 'border-solid border-2 border-green-400 bg-green-200' : ''} ${isSubmitted && answer !== correctAnswer && answer === submittedAnswer ? 'border-solid border-2 border-red-400 bg-red-200 line-through' : ''}`}
             key={answer}
             id={answer}
             type="button"
