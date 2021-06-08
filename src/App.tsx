b1ed07c8ef406dc7e76fc906b7305614574ea2b8
changangus <63685366+changangus@users.noreply.github.com>
2021-06-08 18:33:44 -0400

    interview feedback testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Quiz from './pages/Quiz';
-import { quizzes } from './data/quizzes'
+import { getMoreQuizzes, quizzes as QUIZ_DATA} from './data/quizzes'
 import { useSelector } from 'react-redux';
 import { RootStore } from './redux/store';
 import { quizState } from './redux/quiz/QuizReducer';
+import { iQuiz } from './types/Quiz';
 
 const App: React.FC = () => {
+  const [quizzes, setQuizzes] = useState<iQuiz[]>(QUIZ_DATA)
   const currentQuiz = useSelector<RootStore, quizState["currentQuiz"]>((state) => state.quiz.currentQuiz)
+  
+  useEffect(() => {
+    const getMoreQuizzesCall = async() => {
+      try {
+        const quizData = await getMoreQuizzes();
+        setQuizzes([...quizzes, ...quizData])
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    if(currentQuiz === quizzes.length) {
+      getMoreQuizzesCall();
+    }
+  }, [currentQuiz])
+
   return (
     <div className="flex flex-col items-center h-screen mx-6 sm:mx-10 my-4">
       <Quiz quiz={quizzes[currentQuiz]}/>
